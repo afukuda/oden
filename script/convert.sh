1cf3a9dbd5035b6fc0f9adbc989572a4521d2925
kyubuns <kyubuns@gmail.com>
2015-03-20 00:11:24 +0900

    outputしてmove

--- script/convert.sh
+++ script/convert.sh
@@ -1,4 +1,4 @@
 echo 'convert start'
 cd `dirname $0`
 cd ../
-coffee -c -o oden.js coffee/oden.coffee
+coffee -c -o output coffee/oden.coffee
---
+++ script/move.sh
@@ -0,0 +1,1 @@
+cp output/oden.js '/Applications/Adobe Photoshop CC 2014/Presets/Scripts'
