81ed7df8fc6738ff45c5f04274830ccc44377c9b
kyubuns <kyubuns@gmail.com>
2015-03-22 17:44:05 +0900

    release

---
+++ OdenImporter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class OdenImporter : AssetPostprocessor
+{
+	void OnPreprocessTexture()
+	{
+		var importer = assetImporter as TextureImporter;
+		if(assetImporter == null) return;
+
+		string infoPath = Path.ChangeExtension(assetPath, ".oden");
+		if(!File.Exists(infoPath)) return;
+
+		string borderInfo = File.ReadAllText(infoPath);
+		var borders = borderInfo.Split(',').Select(s => int.Parse(s.Replace(" px", ""))).ToList();
+		// 0L, 1R, 2T, 3B
+		Vector4 border = new Vector4 (borders[0], borders[3], borders[1], borders[2]);
+		SetBorder(importer, border);
+	}
+
+	void SetBorder(TextureImporter importer, Vector4 border)
+	{
+		importer.textureType = TextureImporterType.Sprite;
+		importer.spriteBorder = border;
+	}
+}
---
+++ dev/script/release.sh
@@ -0,0 +1,4 @@
+cd `dirname $0`
+cd ../../
+cp dev/output/oden.js .
+cp dev/unity/Assets/Editor/OdenImporter.cs .
